3a26027f33ebfb73af172f79b8089e1d2976bbb0
Saurabh Shukla <saurabhshukla398@gmail.com>
2025-08-17 22:19:00 +0530

    Added Parsing Program for Mathematical Expressions

---
+++ Project/MathematicalExpParser.c
@@ -0,0 +1,207 @@
+/*  
+
+A Grammar to parse mathematical expression !
+
+1. E -> E + T | E - T | T
+2. T -> T * F | T / F | F
+3. F -> ( E ) | INTEGER | DECIMAL | VAR 
+
+
+Now remove left recursion from it :
+
+1. E -> E + T | E - T | T
+
+E ->  T E'
+E' -> + T E' | - T E' |  $   ( $ represents epsilon)
+
+4. T -> T * F | T / F | F
+
+T -> F T'
+T' -> * F T' |   / F T'  |  $
+
+Combining Everything , final grammar is :
+1. E -> T E'
+2. E' -> + T E' | - T E' | $
+3. T -> F T'
+4. T' -> * F T' | / F T' | $
+5. F -> ( E ) | INTEGER | DECIMAL | VAR 
+
+*/
+
+#include <stdio.h>
+#include<string.h>
+#include "MexprEnums.h"
+#include "ParserExport.h"
+
+parse_rc_t E();
+static parse_rc_t E_dash();
+static parse_rc_t T();
+static parse_rc_t T_dash();
+static parse_rc_t F();
+
+
+// F -> ( E ) | INTEGER | DECIMAL | VAR
+parse_rc_t F(){
+    parse_init();
+    int initial_lchkp;
+    CHECKPOINT(initial_lchkp);
+    // F -> ( E )
+    do{
+        token_code = cyylex();
+        if(token_code != MATH_CPP_BRACKET_START){
+            break;
+        }
+        err = E();
+        if(err == PARSE_ERR) {
+            break;
+        }
+        token_code = cyylex();
+        if(token_code != MATH_CPP_BRACKET_END){ 
+            break;
+        }
+        RETURN_PARSE_SUCCESS;
+    }while(0);
+    RESTORE_CHKP(initial_lchkp);
+
+    // F -> INTEGER | DECIMAL | VAR
+    do{
+        token_code = cyylex();
+        if(token_code == MATH_CPP_INT || token_code == MATH_CPP_DOUBLE || 
+            token_code == MATH_CPP_VARIABLE){
+            RETURN_PARSE_SUCCESS;
+        } 
+        RETURN_PARSE_ERROR;
+    }while(0);
+    RETURN_PARSE_ERROR;
+}
+
+
+
+
+
+// T' -> * F T' | / F T' | $
+parse_rc_t T_dash() {
+    parse_init();
+
+    int initial_lchkp;
+    CHECKPOINT(initial_lchkp);
+    // T' -> * F T'
+    do{
+        token_code = cyylex();
+        if(token_code != MATH_CPP_MUL){
+            break;
+        }
+        err = F();
+        if(err == PARSE_ERR) {
+            break;
+        }
+        err = T_dash();
+        if(err == PARSE_ERR) {  
+            break;
+        }
+        RETURN_PARSE_SUCCESS;
+    }while(0);
+    RESTORE_CHKP(initial_lchkp);
+
+    // T' -> / F T'
+    do{
+        token_code = cyylex();
+        if(token_code != MATH_CPP_DIV){
+            break;
+        }
+        err = F();
+        if(err == PARSE_ERR) {
+            break;
+        }
+        err = T_dash();
+        if(err == PARSE_ERR) {  
+            break;
+        }
+        RETURN_PARSE_SUCCESS;   
+    }while(0);
+    RESTORE_CHKP(initial_lchkp);
+
+    // T' -> $
+    RETURN_PARSE_SUCCESS;
+}
+
+
+
+// E' -> + T E' | - T E' | $
+parse_rc_t E_dash(){
+    parse_init();
+
+    int initial_lchkp;
+    CHECKPOINT(initial_lchkp);
+    // E' -> + T E'
+    do{
+        token_code = cyylex();
+        if(token_code != MATH_CPP_PLUS){
+            break;
+        }
+        err = T();
+        if(err == PARSE_ERR) {
+            break;
+        }
+        err = E_dash();
+        if(err == PARSE_ERR) {  
+            break;
+        }
+        RETURN_PARSE_SUCCESS;
+    }while(0);
+    RESTORE_CHKP(initial_lchkp);
+
+    // E' -> - T E'
+    do{
+        token_code = cyylex();
+        if(token_code != MATH_CPP_MINUS){
+            break;
+        }
+        err = T();
+        if(err == PARSE_ERR) {
+            break;
+        }
+        err = E_dash();
+        if(err == PARSE_ERR) {  
+            break;
+        }
+        RETURN_PARSE_SUCCESS;   
+    }while(0);
+    RESTORE_CHKP(initial_lchkp);
+
+    // E' -> $
+    RETURN_PARSE_SUCCESS;
+}
+
+
+
+//T -> F T'
+parse_rc_t T(){
+    parse_init();
+
+    err = F();
+    if(err == PARSE_ERR) {  
+        RETURN_PARSE_ERROR;
+    }
+    err = T_dash();
+    if(err == PARSE_ERR) {
+        RETURN_PARSE_ERROR;
+    }
+    RETURN_PARSE_SUCCESS;
+}
+
+
+// E -> T E'
+parse_rc_t E() {
+    parse_init();
+
+    err = T();
+    if(err == PARSE_ERR) {
+        RETURN_PARSE_ERROR;
+    }
+    err = E_dash();
+    if(err == PARSE_ERR) {
+        RETURN_PARSE_ERROR;
+    }
+    RETURN_PARSE_SUCCESS;
+}
